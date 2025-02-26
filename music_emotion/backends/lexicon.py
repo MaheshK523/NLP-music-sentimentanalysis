608cc95c01647c4e3c3886c65ff193149e940451
mahesh <mahesh.k.ca.us@gmail.com>
2025-02-26 09:35:34 +0530

    Structure processing logic in lexicon.py

--- music_emotion/backends/lexicon.py
+++ music_emotion/backends/lexicon.py
@@ -8,3 +8,13 @@ from ..models import BackendPrediction
 
 
 EMOTIONS = ("anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise")
+
+LEXICON: dict[str, tuple[tuple[str, float], ...]] = {
+    "afraid": (("fear", 1.5),),
+    "amazed": (("surprise", 1.4), ("joy", 0.4)),
+    "angry": (("anger", 1.6),),
+    "bright": (("joy", 1.0),),
+    "broken": (("sadness", 1.4),),
+    "burn": (("anger", 1.0),),
+    "calm": (("neutral", 1.0), ("joy", 0.3)),
+    "celebrate": (("joy", 1.5),),
