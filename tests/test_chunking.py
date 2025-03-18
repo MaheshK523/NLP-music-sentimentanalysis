4955f6cde2486820df43f0d05f90d5737745fc72
mahesh <mahesh.k.ca.us@gmail.com>
2025-03-18 09:52:07 +0530

    Structure processing logic in test_chunking.py

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -8,3 +8,13 @@ class FakeTokenizer:
         return 2
 
     def encode(self, text, add_special_tokens=False):
+        return list(range(1, len(text.split()) + 1))
+
+    def decode(self, token_ids, skip_special_tokens=True):
+        return " ".join(f"token-{token_id}" for token_id in token_ids)
+
+
+class ChunkingTest(unittest.TestCase):
+    def test_word_tokenization_is_unicode_and_contraction_aware(self):
+        self.assertEqual(tokenize_words("Café, DON'T stop 42 times."), ["café", "don't", "stop", "times"])
+
