81624b0c3dca0dc7aa1dcf3d59b843116a5fb301
mahesh <mahesh.k.ca.us@gmail.com>
2025-04-19 05:07:44 +0530

    Structure processing logic in SentimentAnalysisPreProcess.py

--- legacy/SentimentAnalysisPreProcess.py
+++ legacy/SentimentAnalysisPreProcess.py
@@ -8,3 +8,13 @@ mood_classifier = pipeline("text-classification", model="j-hartmann/emotion-engl
 #function to preprocess lyrics
 def preprocess_lyrics(text):
     text = text.lower()
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+#function to get moods and scores
+def get_song_mood(lyrics, threshold=0.25):
+    lyrics_clean = preprocess_lyrics(lyrics)
+    results = mood_classifier(lyrics_clean)
+    mood_scores = {res['label']: res['score'] for res in results[0]}
+    dominant_moods = [label for label, score in mood_scores.items() if score >= threshold]
